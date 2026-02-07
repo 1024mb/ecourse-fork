efb0c6e882f208543828f5b695edfabef0766c8d
Ángel <1024mb@users.noreply.github.com>
2026-02-07 14:47:37 -0500

    fix(pb): progress hook not setting correct status value
    TypeScript had to be configured to exclude emitting the *.pb.ts files
    because, at least in dev, pocketbase supports *.ts files not only for
    migrations but also for hooks.

---
+++ pb/pb_hooks/utils.ts
@@ -0,0 +1,19 @@
+module.exports = {
+    getProgressTypeId: ({ progressTypeName }: {
+        progressTypeName: string
+    }): string | undefined => {
+        const progressTypeRecords = $app.findAllRecords("progress_type");
+
+        for (const record of progressTypeRecords) {
+            if (record == null) {
+                continue;
+            }
+
+            if (record.getString("type_name") === progressTypeName) {
+                return record.id;
+            }
+        }
+
+        throw new Error("Unknown progress type name " + progressTypeName);
+    },
+};
